d1611712d5894296ee9f3389723f73c4020e63e5
martin Lindahl <martin.li@glimakra.se>
2021-08-15 20:33:33 +0200

    jsxArrayFromMap

--- src/App.js
+++ src/App.js
@@ -6,13 +6,22 @@ function App() {
 
 
   console.log('data', data, typeof data)
+
+  // let jsxArray = [
+  //   <DisplayMovieTitle title={data.films[0]} />,
+  //     <DisplayMovieTitle title={data.films[1]} />,
+  //     <DisplayMovieTitle title={data.films[2]} />,
+  //     <DisplayMovieTitle title={data.films[3]} />
+  // ]
+
+  let jsxArrayFromMap = data.films.map((filmLink) => {
+    // console.log('x', x)
+    return <DisplayMovieTitle title={filmLink} />
+  })
   return (
     <div className="App">
       <h1>Martins testapp</h1>
-      <DisplayMovieTitle title={data.films[0]} />
-      <DisplayMovieTitle title={data.films[1]} />
-      <DisplayMovieTitle title={data.films[2]} />
-      <DisplayMovieTitle title={data.films[3]} />
+      {jsxArrayFromMap}
     </div>
   );
 }
