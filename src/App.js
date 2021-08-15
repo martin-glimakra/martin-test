934261ade8a413aa11bb89ab20dfee4ed3775918
martin Lindahl <martin.li@glimakra.se>
2021-08-15 20:49:29 +0200

    data kommer in från starwarsEpisodes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import DisplayMovieTitle from './components/DisplayMovieTitle';
 import data from './testdata/data.json'
+import starwarsEpisodes from './testdata/starwarsEpisodes.json'
 
 function App() {
 
+  console.log('starwarsEpisodes', starwarsEpisodes)
 
-  console.log('data', data, typeof data)
+
+  // console.log('data', data, typeof data)
 
   // let jsxArray = [
   //   <DisplayMovieTitle title={data.films[0]} />,
@@ -14,10 +17,8 @@ function App() {
   //     <DisplayMovieTitle title={data.films[3]} />
   // ]
 
-  let jsxArrayFromMap = data.films.map((filmLink) => {
-    // console.log('x', x)
-    return <DisplayMovieTitle title={filmLink} />
-  })
+  let jsxArrayFromMap = data.films.map((filmLink) => <DisplayMovieTitle title={filmLink} />)
+  
   return (
     <div className="App">
       <h1>Martins testapp</h1>
